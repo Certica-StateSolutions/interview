043a1d2e119a8e50dc338272f8c571c54fc9f488
Terrimane Pritchett <shon.pritchett@gmail.com>
2019-02-17 20:52:19 -0600

    Adding the exception which will be thrown the attempt to add  a country to the repository if a country already exists which correlates to the same country code.

---
+++ Interview-Exercise/CountryAlreadyExistsException.cs
@@ -0,0 +1,14 @@
+﻿using Interview_Exercise;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterviewExercise
+{
+    public class CountryAlreadyExistsException : Exception
+    {
+        public CountryAlreadyExistsException() { }
+
+        public CountryAlreadyExistsException(Country country) : base(string.Format("Country already exists. Country Code: {0}, Country Name: {1}", country.Code, country.Name)) { }
+    }
+}
