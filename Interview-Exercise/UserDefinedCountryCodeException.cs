4f7024f1623e2bdab0076c2f08e5854831af0fa6
Terrimane Pritchett <shon.pritchett@gmail.com>
2019-02-17 21:08:35 -0600

    Adding the exception which will be thrown when a user defined user code is used for a country code.

---
+++ Interview-Exercise/UserDefinedCountryCodeException.cs
@@ -0,0 +1,16 @@
+﻿using Interview_Exercise;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterviewExercise
+{
+    public class UserDefinedCountryCodeException : Exception
+    {
+        public UserDefinedCountryCodeException() { }
+
+        public UserDefinedCountryCodeException(string countryCode) : base(string.Format("User defined country code is not supported: {0}", countryCode)) { }
+
+        public UserDefinedCountryCodeException(Country country) : base(string.Format("User defined country code is not supported: {0}", country.Code)) { }
+    }
+}
