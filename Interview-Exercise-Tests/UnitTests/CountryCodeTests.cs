0eb61651f5128ed1a75910fc0a7aeb5c7ab4c2ab
Terrimane Pritchett <shon.pritchett@gmail.com>
2019-02-17 20:47:28 -0600

    Adding unit tests for the Country class.

---
+++ Interview-Exercise-Tests/UnitTests/CountryCodeTests.cs
@@ -0,0 +1,29 @@
+﻿using Interview_Exercise;
+using NUnit.Framework;
+
+namespace InterviewExercise.Tests.UnitTests
+{
+    [TestFixture]
+    class CountryCodeTests : TestFixtureBase
+    {
+        private Country _country;
+        private string _expectedResult;
+
+        protected override void Arrange()
+        {
+            _country = new Country();
+            _expectedResult = "ABC";
+        }
+
+        protected override void Act()
+        {
+            _country.Code = "AbC";
+        }
+
+        [Test]
+        public void Code_is_converted_to_upper_case()
+        {
+            Assert.That(_country.Code == _expectedResult);
+        }
+    }
+}
