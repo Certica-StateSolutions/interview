87613133197d5347aea3e9bb1808b855a3574d30
Terrimane Pritchett <shon.pritchett@gmail.com>
2019-02-17 20:21:24 -0600

    Adding the Country class.  Note - county codes can provided using uppercase, lowercase, or a mixture thereof, however the CountryCode is stored in uppercase only.

--- Interview-Exercise/Country.cs
+++ Interview-Exercise/Country.cs
@@ -1,7 +1,39 @@
-﻿namespace Interview_Exercise
+﻿using InterviewExercise;
+
+namespace Interview_Exercise
 {
     public class Country
     {
-        
+        string _code;
+
+        public string Code
+        {
+            get
+            {
+                return _code;
+            }
+
+            set
+            {
+                if (string.IsNullOrEmpty(value) == false)
+                {
+                    _code = value.ToUpper();
+                }
+            }
+        }
+
+        public string Name { get; set; }
+
+        public Country()
+        {
+            _code = string.Empty;
+            Name = string.Empty;
+        }
+
+        public Country(string code, string name)
+        {
+            Code = code;
+            Name = name;
+        }
     }
 }
