6b5ea95421254ec2b1de44637af322f6043322c3
Terrimane Pritchett <shon.pritchett@gmail.com>
2019-02-17 20:17:53 -0600

    Adding UserDefinedCountryCodeValidatorTests file which has unit tests for the UserDefinedCountryCodeValidator class.

---
+++ Interview-Exercise-Tests/UnitTests/UserDefinedCountryCodeValidatorTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+
+namespace InterviewExercise.Tests.UnitTests
+{
+    [TestFixture]
+    class InvalidCountryCodeTest : TestFixtureBase
+    {
+        private bool _actualResult;
+        private ICountryCodeValidator iCountryCodeValidator;
+
+        protected override void Arrange()
+        {
+            iCountryCodeValidator = new UserDefinedCountryCodeValidator();
+        }
+
+        protected override void Act()
+        {
+            _actualResult = iCountryCodeValidator.IsCountryCodeValid("AAZ");
+        }
+
+        [Test]
+        public void Result_is_false()
+        {
+            Assert.That(_actualResult, Is.False);
+        }
+    }
+
+    class ValidCountryCodeTest : TestFixtureBase
+    {
+        private bool _actualResult;
+        private ICountryCodeValidator iCountryCodeValidator;
+
+        protected override void Arrange()
+        {
+            iCountryCodeValidator = new UserDefinedCountryCodeValidator();
+        }
+
+        protected override void Act()
+        {
+            _actualResult = iCountryCodeValidator.IsCountryCodeValid("BBZ");
+        }
+
+        [Test]
+        public void Result_is_true()
+        {
+            Assert.That(_actualResult, Is.True);
+        }
+    }
+}
