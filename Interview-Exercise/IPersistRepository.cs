009f8db6fd131631d8e644e006b107affd774be9
Terrimane Pritchett <shon.pritchett@gmail.com>
2019-02-17 21:17:16 -0600

    Adding the IPersistRepository interface.  While similar to the IRepositoy interface the name is meant to imply that persistence is to be handled separately from the behavior of the repository itself.  As an example if you review the CountryCodeRepository class you will note that a dictionary is used.  This is roughly the equivalent of an in memory cache or database which is typically employed to reduce the latency of queries.  Also, there is now the potential to write to persistent storage asynchronously a leverage the benefits of eventual consistency.

---
+++ Interview-Exercise/IPersistRepository.cs
@@ -0,0 +1,13 @@
+﻿using Interview_Exercise;
+
+namespace InterviewExercise
+{
+    public interface IPersistRepository
+    {
+        void Add(Country country);
+        void Update(Country country);
+        void Delete(string countryCode);
+        Country Get(string countryCode);
+        void Clear();
+    }
+}
