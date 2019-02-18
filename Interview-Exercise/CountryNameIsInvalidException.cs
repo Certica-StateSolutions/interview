a99a727d99d776bc672a185d9f39908bfdcfa474
Terrimane Pritchett <shon.pritchett@gmail.com>
2019-02-17 21:04:41 -0600

    Adding the exception which will be thrown when there is an attempt to add a country to the repository that does have a code defined but does not have a valid name defined for the country.

---
+++ Interview-Exercise/CountryNameIsInvalidException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace InterviewExercise
+{
+    public class CountryNameIsInvalidException : Exception
+    {
+        public CountryNameIsInvalidException() : base("Country name is null or empty.") { }
+    }
+}
