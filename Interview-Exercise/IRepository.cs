5381b110bea58c1e17f49c8952cd92ebb0dc7815
Terrimane Pritchett <shon.pritchett@gmail.com>
2019-02-17 20:49:37 -0600

    Adding an the IRepository interface to codify the contract for the country code repository.  The API can now be maintained separately from the implementation of the contract.

---
+++ Interview-Exercise/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using Interview_Exercise;
+
+namespace InterviewExercise
+{
+    public interface IRepository
+    {
+        void Add(Country country);
+        void Update(Country country);
+        void Delete(string countryCode);
+        Country Get(string countryCode);
+        void Clear();
+    }
+}
