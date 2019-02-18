5d0c58994628d6e7faa7296733829f47bbb4ad86
Terrimane Pritchett <shon.pritchett@gmail.com>
2019-02-17 21:03:36 -0600

    Adding the exceptions which will be thrown when a country cannot be located by its code in the repository.

---
+++ Interview-Exercise/CountryNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿using Interview_Exercise;
+using System;
+
+namespace InterviewExercise
+{
+    public class CountryNotFoundException : Exception
+    {
+        public CountryNotFoundException() { }
+        public CountryNotFoundException(string countryCode) : base(string.Format("Country cannot be found. Country Code: {0}", countryCode)) { }
+    }
+}
